be479d2ac4bca06d5ec1042c104c9087f7a42d34
huy.nguyen <nguyenquanghuy031295@gmail.com>
2025-08-02 13:32:29 +0700

    feat: enable load static files for media-server

--- src/SCS.MediaServer/Program.cs
+++ src/SCS.MediaServer/Program.cs
@@ -1,9 +1,15 @@
 using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateSlimBuilder(args);
+var configuration = builder.Configuration;
+var cameraHLSFolder = configuration.GetValue<string>("CameraHLSFolder");
 
 var app = builder.Build();
 
-app.UseStaticFiles();
+app.UseStaticFiles(new StaticFileOptions
+{
+    FileProvider = new PhysicalFileProvider(cameraHLSFolder),
+    RequestPath = "/hls",
+});
 
 app.Run();
