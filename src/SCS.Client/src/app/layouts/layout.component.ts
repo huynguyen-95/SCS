e4754269a9c6d8fe2b96cb35d3b726099dfb772f
huy.nguyen <nguyenquanghuy031295@gmail.com>
2025-08-11 18:04:21 +0700

    feat: add menu with sub items

--- src/SCS.Client/src/app/layouts/layout.component.ts
+++ src/SCS.Client/src/app/layouts/layout.component.ts
@@ -1,18 +1,32 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
+import { ButtonModule } from 'primeng/button';
+import { Ripple, RippleModule } from 'primeng/ripple';
+import { AvatarModule } from 'primeng/avatar';
+import { StyleClass } from 'primeng/styleclass';
+import { DrawerModule } from 'primeng/drawer';
 
 @Component({
     selector: 'app-layout',
     templateUrl: './layout.component.html',
     styleUrls: ['./layout.component.scss'],
     standalone: true,
-    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive]
+    imports: [CommonModule, RouterOutlet, RouterLink, RouterLinkActive, DrawerModule, ButtonModule, Ripple, AvatarModule, StyleClass]
 })
 export class LayoutComponent {
     sidebarVisible: boolean = true;
+    expandedMenus: { [key: string]: boolean } = {};
 
     toggleSidebar(): void {
         this.sidebarVisible = !this.sidebarVisible;
+    }
+
+    toggleSubmenu(menuKey: string): void {
+        this.expandedMenus[menuKey] = !this.expandedMenus[menuKey];
+    }
+
+    isSubmenuExpanded(menuKey: string): boolean {
+        return this.expandedMenus[menuKey] || false;
     }
 }
