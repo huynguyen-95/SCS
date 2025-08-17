199a16dff1c689c91389fa7653074a507e594787
huy.nguyen <nguyenquanghuy031295@gmail.com>
2025-08-17 16:06:44 +0700

    fix: streaming player by adding id input

--- src/SCS.Client/src/app/components/streaming-player/streaming-player.ts
+++ src/SCS.Client/src/app/components/streaming-player/streaming-player.ts
@@ -1,4 +1,4 @@
-import { AfterViewInit, Component, OnInit, signal, WritableSignal } from '@angular/core';
+import { AfterViewInit, Component, input, OnInit, signal, WritableSignal } from '@angular/core';
 import HlsJs from 'hls.js';
 
 @Component({
@@ -10,6 +10,11 @@ import HlsJs from 'hls.js';
 export class StreamingPlayer implements OnInit, AfterViewInit {
   public sourceUrl: string = 'https://scs-assessment-bucket.s3.ap-southeast-1.amazonaws.com/fl1-1/playlist.m3u8';
   public showError: WritableSignal<boolean> = signal(false);
+  public id = input.required<number>();
+
+  get videoId(): string {
+    return `player-${this.id()}`;
+  }
 
   ngOnInit(): void {
   }
@@ -20,11 +25,14 @@ export class StreamingPlayer implements OnInit, AfterViewInit {
       console.error('HLS is not supported in this browser.');
       return;
     }
-    const video: HTMLVideoElement = document.getElementById('player') as HTMLVideoElement;
+
+    const video: HTMLVideoElement = document.getElementById(this.videoId) as HTMLVideoElement;
     var hls = new HlsJs();
     hls.loadSource(this.sourceUrl);
     hls.attachMedia(video);
+
     hls.on(HlsJs.Events.MANIFEST_PARSED, function () {
+      video.muted = true; // Mute the video to avoid autoplay restrictions
       video.play();
     });
 
